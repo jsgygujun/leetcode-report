47ffc1e59b6fe96416f6cb03fc2352b6003ba87c
jun <jsgygujun@gmail.com>
2020-06-17 22:04:56 +0800

    update.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# leetcode-report
-LeetCode 解题报告
+# LeetCode 解题报告
+## 目录
+
+- [00001-两数只和](/src/main/java/jsgygujun/code/_00001/README.md) 
+- 
---
+++ src/main/java/com/jsgygujun/code/_00001/Solution.java
@@ -0,0 +1,7 @@
+package com.jsgygujun.code._00001;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[]{0, 0};
+    }
+}
