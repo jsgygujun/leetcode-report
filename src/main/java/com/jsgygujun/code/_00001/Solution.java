3705cc8a0b29667cc34c3cc2b452f261170a4ec0
jun <jsgygujun@gmail.com>
2020-06-17 22:31:23 +0800

    add 00001-TwoSum.

--- src/main/java/com/jsgygujun/code/_00001/Solution.java
+++ src/main/java/com/jsgygujun/code/_00001/Solution.java
@@ -1,7 +1,26 @@
 package com.jsgygujun.code._00001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return new int[]{0, 0};
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length);
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target-nums[i])) {
+                return new int[]{i, map.get(target-nums[i])};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] res = sol.twoSum(nums, target);
+        System.out.println(res[0] + ", " + res[1]);
     }
 }
