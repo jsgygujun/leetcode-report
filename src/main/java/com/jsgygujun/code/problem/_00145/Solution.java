b9f451a639545b06f9a30b0d636edbc1f4ef92ed
jsgygujun <jsgygujun@gmail.com>
2020-06-18 12:01:26 +0800

    添加"颜色标记法"非递归后序遍历二叉树。

--- src/main/java/com/jsgygujun/code/problem/_00145/Solution.java
+++ src/main/java/com/jsgygujun/code/problem/_00145/Solution.java
@@ -4,6 +4,7 @@ import com.jsgygujun.code.util.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author gujun@qiyi.com
@@ -15,10 +16,44 @@ public class Solution {
         postorderHelper(root, res);
         return res;
     }
+    // 递归遍历
     private void postorderHelper(TreeNode root, List<Integer> res) {
         if (root == null) return;
         postorderHelper(root.left, res);
         postorderHelper(root.right, res);
         res.add(root.val);
+    }
+
+    // 节点颜色
+    enum NodeColor {WHITE, GRAY}
+    // 彩色节点
+    private static class ColorNode {
+        private NodeColor color;
+        private TreeNode node;
+        public ColorNode(NodeColor color, TreeNode node) {
+            this.color = color;
+            this.node = node;
+        }
+    }
+    // 颜色标记法
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<ColorNode> stack = new Stack<>();
+        stack.push(new ColorNode(NodeColor.WHITE, root));
+        while (!stack.isEmpty()) {
+            ColorNode colorNode = stack.pop();
+            NodeColor color = colorNode.color;
+            TreeNode node = colorNode.node;
+            if (node == null) continue;
+            if (color == NodeColor.WHITE) {
+                stack.add(new ColorNode(NodeColor.GRAY, node)); // 中
+                stack.add(new ColorNode(NodeColor.WHITE, node.right)); // 右
+                stack.add(new ColorNode(NodeColor.WHITE, node.left)); // 左
+            } else {
+                res.add(node.val);
+            }
+        }
+        return res;
     }
 }
