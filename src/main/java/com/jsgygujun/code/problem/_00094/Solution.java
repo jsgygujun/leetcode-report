c1f50ff1d4e5728e2310197729472a9989ddbccd
jsgygujun <jsgygujun@gmail.com>
2020-06-18 11:24:55 +0800

    update.

--- src/main/java/com/jsgygujun/code/problem/_00094/Solution.java
+++ src/main/java/com/jsgygujun/code/problem/_00094/Solution.java
@@ -1,8 +1,24 @@
 package com.jsgygujun.code.problem._00094;
 
+import com.jsgygujun.code.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author gujun@qiyi.com
  * @since 2020/6/18 10:52 上午
  */
 public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderHelper(root, res);
+        return res;
+    }
+    private void inorderHelper(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        inorderHelper(root.left, res);
+        res.add(root.val);
+        inorderHelper(root.right, res);
+    }
 }
