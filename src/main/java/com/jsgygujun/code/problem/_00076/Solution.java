ab47adea6aa2440e1a9fb41d83a953180dbfcae1
jun <jsgygujun@gmail.com>
2020-06-30 23:12:19 +0800

    添加76-最小覆盖子串.

---
+++ src/main/java/com/jsgygujun/code/problem/_00076/Solution.java
@@ -0,0 +1,74 @@
+package com.jsgygujun.code.problem._00076;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * 76-最小覆盖子串
+ *
+ * 题目：
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ *
+ * 示例：
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ *
+ * 说明：
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ */
+public class Solution {
+
+    Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        for (int r = 0; r < s.length(); ++r) {
+            if (ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void test_1() {
+        Solution sol = new Solution();
+        Assert.assertEquals("", sol.minWindow("", ""));
+        Assert.assertEquals("BANC", sol.minWindow("ADOBECODEBANC", "ABC"));
+    }
+}
