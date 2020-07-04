88a7d30d074e873d63ee9cf2b75f6ea4f13e76b8
jun <jsgygujun@gmail.com>
2020-07-04 22:21:30 +0800

    add 101. 对称二叉树

---
+++ src/main/java/com/jsgygujun/code/problem/_00101_00200/_00101/Solution.java
@@ -0,0 +1,62 @@
+package com.jsgygujun.code.problem._00101_00200._00101;
+
+import com.jsgygujun.code.util.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * 101. 对称二叉树
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return root1.val == root2.val && isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        Assert.assertTrue(new Solution().isSymmetric(root));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+        Assert.assertFalse(new Solution().isSymmetric(root));
+    }
+
+}
