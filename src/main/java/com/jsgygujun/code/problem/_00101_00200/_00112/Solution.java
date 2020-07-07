431542be636691b52dd1f74c7764f64c5c9762b1
jun <jsgygujun@gmail.com>
2020-07-07 22:21:42 +0800

    add 112. 路径总和

---
+++ src/main/java/com/jsgygujun/code/problem/_00101_00200/_00112/Solution.java
@@ -0,0 +1,48 @@
+package com.jsgygujun.code.problem._00101_00200._00112;
+
+import com.jsgygujun.code.util.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 63. 不同路径 II
+ *
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ *
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
+ */
+public class Solution {
+    // DFS
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == sum;
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+        Assert.assertTrue(new Solution().hasPathSum(root, 22));
+    }
+}
