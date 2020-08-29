b5a493cd1c122f8c112d6d99babbd25a6d649337
jun <jsgygujun@gmail.com>
2020-08-29 08:33:23 +0800

    [ADD] 19. 删除链表的倒数第N个节点

--- src/main/java/com/jsgygujun/code/problem/_00001_00100/_00019/Solution.java
+++ src/main/java/com/jsgygujun/code/problem/_00001_00100/_00019/Solution.java
@@ -2,8 +2,29 @@ package com.jsgygujun.code.problem._00001_00100._00019;
 
 import com.jsgygujun.code.util.ListNode;
 
+
+/**
+ * 19. 删除链表的倒数第N个节点
+ *
+ * 题目：
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ * 说明：
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ * 你能尝试使用一趟扫描实现吗？
+ */
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        /*
+         * - 快慢指针
+         * 快指针从链表头向前移动`n+1`步，慢指针从链表头出发。同时移动快慢指针，当快指针到达最后一个节点时，则当慢指针的删除其next节点。
+         */
         ListNode dummy = new ListNode(0); // 哑节点：避开对头节点删除时的特殊处理
         dummy.next = head;
         ListNode fast = dummy, slow = dummy;
