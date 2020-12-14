119628ba88c32444c13ea4a2dd736931610de5db
jun <jsgygujun@gmail.com>
2020-12-14 23:06:58 +0800

    [UPDATE] 617. 合并二叉树

--- src/main/java/com/jsgygujun/code/problem/_00601_00700/_00617_合并二叉树/Solution.java
+++ src/main/java/com/jsgygujun/code/problem/_00601_00700/_00617_合并二叉树/Solution.java
@@ -1,4 +1,4 @@
-package com.jsgygujun.code.problem._00601_00700._00617;
+package com.jsgygujun.code.problem._00601_00700._00617_合并二叉树;
 
 import com.jsgygujun.code.util.TreeNode;
 
@@ -7,9 +7,9 @@ import com.jsgygujun.code.util.TreeNode;
  *
  * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
  *
- * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为「NULL」的节点将直接作为新二叉树的节点。
  *
- * 示例 1:
+ * 示例1:
  * 输入:
  * 	      Tree 1                     Tree 2
  *           1                         2
@@ -33,8 +33,8 @@ public class Solution {
         if (t1 == null) return t2;
         if (t2 == null) return t1;
         TreeNode node = new TreeNode(t1.val + t2.val); // Merge之后的节点
-        node.left = mergeTrees(t1.left, t2.left);
-        node.right = mergeTrees(t1.right, t2.right);
+        node.left = mergeTrees(t1.left, t2.left); // 递归完成左边
+        node.right = mergeTrees(t1.right, t2.right); // 递归完成右边
         return node;
     }
 }
